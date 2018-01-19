b0b9c8b25ac44315a84414ce1324dd7b5c6b4c44
Pablo Sabater Canseco <pablo.sabater@metrica.es>
2018-01-19 15:36:56 +0100

    Cambiada la forma de relacionarse los modelos entre si.

--- common/models/empleado.js
+++ common/models/empleado.js
@@ -2,4 +2,25 @@
 
 module.exports = function(Empleado) {
 
+    Empleado.pdf = function(nombre, cb) {
+
+        Empleado.find({where: {nombre: nombre}}, function(err, empleados) {
+            console.log(empleados[0]);
+        
+        
+        
+        });
+
+
+
+        cb(null, 'Greetings... ' + nombre);
+      }
+  
+      Empleado.remoteMethod('pdf', {
+            accepts: {arg: 'nombre', type: 'string'},
+            returns: {arg: 'greeting', type: 'string'},
+            http:{path:'/:nombre/pdf', verb: 'get'}
+      });
+
+
 };
